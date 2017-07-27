c089d0fbb032f1d7af69c9b2fa6d01520f056997
alimasyhur <jegrag4ever@gmail.com>
2017-07-27 07:44:35 +0700

    solved case 2 of 608

--- calculation.go
+++ calculation.go
@@ -10,11 +10,32 @@ func findOdd(numbers int) []int {
 	return newArr
 }
 
+//SumOddNumber sum int: case One
 func SumOddNumber(numbers int) int {
-  array := findOdd(numbers)
+	array := findOdd(numbers)
 	sum := 0
 	for _, value := range array {
 		sum += value
+	}
+	return sum
+}
+
+//Fibonacci n int
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+//CalculateOddNumberFibonacci n: Case Two
+func CalculateOddNumberFibonacci(n int) int {
+	sum := 0
+	for index := 1; index <= n; index++ {
+		fibonacci := Fibonacci(index)
+		if fibonacci%2 != 0 {
+			sum += fibonacci
+		}
 	}
 	return sum
 }
